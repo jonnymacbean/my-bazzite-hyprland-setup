13984fab103cecfb914a4fdcffdd80572ffec620
jonnymacbean <75743578+jonnymacbean@users.noreply.github.com>
2025-12-02 16:39:56 +0000

    /opt

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,7 +4,12 @@ set -ouex pipefail
 
 # Install repos
 dnf config-manager addrepo -y --from-repofile=https://repo.librewolf.net/librewolf.repo
+dnf config-manager addrepo --from-repofile=https://repository.mullvad.net/rpm/beta/mullvad.repo
 dnf -y copr enable solopasha/hyprland
+
+# Enable writing to /opt
+rm /opt
+mkdir /opt
 
 
 # install packages
@@ -23,14 +28,12 @@ dnf install -y --allowerasing \
   waypaper \
   hyprpolkitagent \
   hyprsysteminfo \
-  alacritty
-
-
-# install Mullvad VPN
-wget -O mullvad-vpn.rpm --trust-server-names https://mullvad.net/download/app/rpm/latest
-dnf install -y ./mullvad-vpn.rpm
+  alacritty \
+  mullvad-vpn
 
 # Cleanup
+mv /opt /usr/share/factory
+ln -s /var/opt /opt
 rm -f mullvad-vpn.rpm
 flatpak uninstall -y --noninteractive org.mozilla.firefox
 dnf -y copr disable solopasha/hyprland
