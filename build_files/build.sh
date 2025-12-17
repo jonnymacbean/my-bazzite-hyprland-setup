85e40b197b9f04387a8a4c59841e9eecec764ce5
jonnymacbean <75743578+jonnymacbean@users.noreply.github.com>
2025-12-17 11:23:04 +0000

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -94,9 +94,9 @@ cd ../..
 git clone -b master --depth 1 https://github.com/keyitdev/sddm-astronaut-theme.git /usr/share/sddm/themes/sddm-astronaut-theme
 cp -r /usr/share/sddm/themes/sddm-astronaut-theme/Fonts/* /usr/share/fonts/
 echo "[Theme]
-Current=sddm-astronaut-theme" | sudo tee /etc/sddm.conf
+Current=sddm-astronaut-theme" | tee /etc/sddm.conf
 echo "[General]
-InputMethod=qtvirtualkeyboard" | sudo tee /etc/sddm.conf.d/virtualkbd.conf
+InputMethod=qtvirtualkeyboard" | tee /etc/sddm.conf.d/virtualkbd.conf
 sed -i 's|ConfigFile=Themes/astronaut.conf|ConfigFile=Themes/black_hole.conf|g' /usr/share/sddm/themes/sddm-astronaut-theme/metadata.desktop
 
   
