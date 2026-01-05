599a07868c0ad6db79841b6267f99bc856710d0f
jonnymacbean <75743578+jonnymacbean@users.noreply.github.com>
2026-01-05 15:06:42 +0000

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -29,7 +29,7 @@ dnf install -y \
   uwsm \
   python-pip \
   python3-gobject \
-  nm-connection-editor \
+  nm-connection-editor@1.36.0-6.fc43 \
   network-manager-applet \
   fuse \
   ImageMagick \
