fcee89d0b50d9fd0ee2554479116d441233a133e
jonnymacbean <75743578+jonnymacbean@users.noreply.github.com>
2026-01-14 19:03:00 +0000

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -6,11 +6,7 @@ set -ouex pipefail
 dnf config-manager addrepo -y --from-repofile=https://repo.librewolf.net/librewolf.repo
 dnf config-manager addrepo --from-repofile=https://repository.mullvad.net/rpm/beta/mullvad.repo
 dnf config-manager addrepo --from-repofile=https://download.opensuse.org/repositories/shells:zsh-users:zsh-completions/Fedora_Rawhide/shells:zsh-users:zsh-completions.repo
-dnf -y copr enable solopasha/hyprland
-dnf -y copr enable peterwu/rendezvous
 dnf -y copr enable lbarrys/cliphist
-dnf -y copr enable tofik/nwg-shell
-dnf -y copr enable erikreider/SwayNotificationCenter
 
 # Enable writing to /opt
 rm /opt
@@ -22,78 +18,17 @@ dnf install -y \
   librewolf \
   keepassxc \
   mullvad-vpn \
-  hyprland \
-  libnotify \
-  qt5-qtwayland \
-  qt6-qtwayland \
-  uwsm \
-  python-pip \
-  python3-gobject \
-  network-manager-applet \
-  nm-connection-editor \
-  NetworkManager-tui \
-  fuse \
-  ImageMagick \
-  waypaper \
-  SwayNotificationCenter \
-  fontawesome-fonts \
-  wget \
-  curl \
-  git \
-  rsync \
-  unzip \
-  jq \
-  flatpak \
-  vim \
-  inotify-tools \
-  hyprpaper \
-  hyprlock \
-  hypridle \
-  hyprpicker \
   hyprpolkitagent \
   hyprutils \
-  xdg-desktop-portal-hyprland \
-  kitty \
-  wlogout \
-  vlc \
-  nwg-dock-hyprland \
-  waybar \
-  rofi-wayland \
-  nwg-look \
-  pavucontrol \
-  neovim \
-  blueman \
-  qt6ct \
-  nautilus \
-  xdg-user-dirs \
-  xdg-desktop-portal-gtk \
-  figlet \
-  fastfetch \
-  htop \
-  xclip \
-  zsh \
-  fzf \
-  brightnessctl \
-  tumbler \
-  slurp \
-  cliphist \
-  gvfs \
-  grim \
-  breeze-gtk \
-  plasma-breeze \
-  qt6-qtsvg \
-  qt6-qtvirtualkeyboard \
-  qt6-qtmultimedia \
   ncdu \
   tealdeer \
   gamemode
 
+# ml4w dotfiles setup
 git clone --depth 1 https://github.com/mylinuxforwork/dotfiles.git dotfiles
-cd dotfiles/setup
-cp packages/eza /usr/bin
-cp scripts/grimblast /usr/bin
-cp -rf fonts/* /usr/share/fonts
-cd ../..
+cd dotfiles
+HOME=/etc/skel setup/setup-fedora.sh
+cp -rf dotfiles/* /etc/skel
 
 # SDDM theme
 git clone -b master --depth 1 https://github.com/keyitdev/sddm-astronaut-theme.git /usr/share/sddm/themes/sddm-astronaut-theme
@@ -107,8 +42,4 @@ sed -i 's|ConfigFile=Themes/astronaut.conf|ConfigFile=Themes/black_hole.conf|g' 
   
 # Cleanup
 rm -rf dotfiles
-dnf -y copr disable solopasha/hyprland
-dnf -y copr disable peterwu/rendezvous
 dnf -y copr disable lbarrys/cliphist
-dnf -y copr disable tofik/nwg-shell
-dnf -y copr disable erikreider/SwayNotificationCenter
